af1ce96c17f78fca18b1af7f01078bf71c6248e3
Ajith Kumar V <ajithajk46@gmail.com>
2025-04-10 09:38:03 +0530

    Update socket.js

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -7,7 +7,7 @@ const server = createServer(app)
 const io = new Server(server, {
     cors: {
         // origin: "http://localhost:2001",
-        origin: "https://chat-app-frontend-mpcj.onrender.com",
+        origin: "*",
         methods: ["GET", "POST", "PUT"],
         credentials: true
     }

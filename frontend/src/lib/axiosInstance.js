49794118b0ffb0f4380f73a67fe3e7a96e9cd243
Ajith Kumar V <ajithajk46@gmail.com>
2025-04-10 09:51:34 +0530

    Update axiosInstance.js

--- frontend/src/lib/axiosInstance.js
+++ frontend/src/lib/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstanace = axios.create({
-    // baseURL: import.meta.env.MODE === "devlopment" ? "http://localhost:2000/api" : "/api",
-    baseURL: "https://chat-app1-66hw.onrender.com/api",
+    baseURL: import.meta.env.MODE === "devlopment" ? "http://localhost:2000/api" : "/api",
+    // baseURL: "https://chat-app1-66hw.onrender.com/api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
